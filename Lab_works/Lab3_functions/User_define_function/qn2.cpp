adeac9f512ca60cdab5820cc303aa9bb92f11ee5
Malisha Maharjan <malishamaharjan00@gmail.com>
2021-07-06 21:52:42 +0545

    Without arguments and no return type

---
+++ Lab_works/Lab3_functions/User_define_function/qn2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void area(float length, float breadth);
+
+int main()
+{
+    float length, breadth;
+
+    cout << "Enter length: ";
+    cin >> length;
+
+    cout << "Enter breadth: ";
+    cin >> breadth;
+
+    area(length, breadth);
+
+    return 0;
+}
+
+void area(float length, float breadth)
+{
+    float ans;
+    ans = (1 / 2.0) * length * breadth;
+    cout << "Area is " << ans << endl;
+}
