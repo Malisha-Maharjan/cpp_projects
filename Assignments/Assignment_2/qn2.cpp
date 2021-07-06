7137d08ce0af23a49baf2c066aa31a041f410278
Malisha Maharjan <malishamaharjan00@gmail.com>
2021-07-06 22:23:55 +0545

    To check positive, negative or zero

---
+++ Assignments/Assignment_2/qn2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "enter a number: ";
+    cin >> num;
+
+    if (num < 0)
+    {
+        cout << "Negative" << endl;
+    }
+    else if (num > 0)
+    {
+        cout << "Positive" << endl;
+    }
+    else{
+        cout << "Zero" << endl;
+    }
+
+    return 0;
+}
