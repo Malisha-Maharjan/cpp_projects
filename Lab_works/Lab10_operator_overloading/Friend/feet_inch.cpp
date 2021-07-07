c715e60d3bc87afd3ae84ab0af7cf77608586b5e
Malisha Maharjan <malishamaharjan00@gmail.com>
2021-07-07 14:22:16 +0545

    Illustration of operator overloading as friend function

---
+++ Lab_works/Lab10_operator_overloading/Friend/feet_inch.cpp
@@ -0,0 +1,55 @@
+//overloaded function takes one parameter
+
+#include <iostream>
+
+using namespace std;
+
+class Distance
+{
+    private:
+    int feet, inch;
+
+    public:
+
+
+    Distance()
+    {
+        feet = 0;
+        inch = 0;
+    }
+
+    Distance(int f, int i)
+    {
+        this->feet = f;
+        this->inch = i;
+    }
+
+    friend Distance operator +(Distance d1, Distance d2);
+
+    void display()
+    {
+        cout << feet << "'" << inch << endl;
+    }
+
+};
+
+Distance operator +(Distance d1, Distance d2)
+{
+    Distance temp;
+
+    temp.feet = d1.feet + d2.feet;
+    temp.inch = d1.inch + d2.inch;
+
+    return temp;
+}
+
+int main()
+{
+    Distance d1 = Distance(10, 2);
+    Distance d2 = Distance(20,4);
+
+    Distance d3 = d1 + d2;
+
+    d3.display();
+
+}
