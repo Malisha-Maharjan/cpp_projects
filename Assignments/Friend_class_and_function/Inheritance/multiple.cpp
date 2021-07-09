958b98ce69381c47dfe5c48b5250ad6cabeb96c0
Malisha Maharjan <malishamaharjan00@gmail.com>
2021-07-09 15:15:22 +0545

    Multiple inheritance

--- Assignments/Friend_class_and_function/Inheritance/multiple.cpp
+++ Assignments/Friend_class_and_function/Inheritance/multiple.cpp
@@ -14,15 +14,15 @@ class Genre
         cin.getline(g, 50);
     }
     
-    // void displayGenre()
-    // {
-    //     cout << "Genre: " << g << endl;
-    // }
-
-    ~Genre()
+    void displayGenre()
     {
         cout << "Genre: " << g << endl;
     }
+
+    // ~Genre() destructor.
+    // {
+    //     cout << "Genre: " << g << endl;
+    // }
 };
 
 class Writter
@@ -37,15 +37,15 @@ class Writter
         cin.getline(name,30);
     }
 
-    // void displayWritter()
-    // {
-    //     cout << "Written by: " << name << endl;
-    // }
-
-    ~Writter()
+    void displayWritter()
     {
         cout << "Written by: " << name << endl;
     }
+
+    // ~Writter()
+    // {
+    //     cout << "Written by: " << name << endl;
+    // }
 };
 
 class Book: public Genre, public Writter
@@ -71,7 +71,13 @@ int main()
 {
     Book book1;
     book1.displayBook();
-    // book1.displayGenre();
-    // book1.displayWritter();
+    book1.displayGenre();
+    book1.displayWritter();
 
+    return 0;
 }
+
+//Note::-> Parent class ko construtor paila call hunxa.
+//      -> Destructor chai sabbai class ko constructor call vaye vani call hunxa.
+//      -> Destructor ma chai last in first out wala principle lagxa.
+//      -> Jun class ko constructor last ma call hunxa tesko destructor first ma call hunxa.
